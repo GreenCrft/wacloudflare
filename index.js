f73c29259e88d11103986c9c6fef512f383d902c
GreenCrft <148884052+GreenCrft@users.noreply.github.com>
2025-06-17 08:48:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ export default {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+    <!-- Open Graph Tags -->
     <meta property="og:title" content="Owner SpielNetwork" />
-    <meta property="og:description" content="Hi, Im SpielNetwork Owner, click the link if u want to talk with me" />
+    <meta property="og:description" content="Hi, I'm SpielNetwork Owner, click the link if u want to talk with me" />
     <meta property="og:image" content="https://i.imgur.com/C3Its49.jpeg" />
-    <meta property="og:url" content="https://greenc.store" />
+    <meta property="og:url" content="https://greenc.tech" />
     <meta name="theme-color" content="#25D366" />
     <meta http-equiv="refresh" content="2; url=https://wa.me/6285813087161" />
 
@@ -20,30 +21,31 @@ export default {
         margin: 0;
         padding: 0;
         height: 100%;
+        width: 100%;
+        font-family: 'Segoe UI', sans-serif;
       }
 
       body {
-        background: url('https://i.imgur.com/C3Its49.jpeg') no-repeat center center fixed;
-        background-size: cover;
-        font-family: 'Segoe UI', sans-serif;
         display: flex;
         justify-content: center;
         align-items: center;
+        background: url('https://i.imgur.com/C3Its49.jpeg') no-repeat center center fixed;
+        background-size: cover;
       }
 
       .text {
-        background-color: rgba(0, 0, 0, 0.6);
-        color: #ffffff;
+        background: rgba(0, 0, 0, 0.6);
+        color: #fff;
         padding: 30px 50px;
         border-radius: 15px;
-        font-size: 2rem;
         box-shadow: 0 0 20px #25D366;
+        font-size: 2rem;
         text-align: center;
       }
     </style>
   </head>
   <body>
-    <div class="text">Sedang Menghubungkan ke WhatsApp <strong>Green</strong>...</div>
+    <div class="text">Menghubungkan Ke <strong>Mr.Green</strong>...</div>
   </body>
 </html>`, {
       status: 200,
